db4d7a705ed713c23393ffa0ec1380c1a1c744b1
Discovery19 <sergeymukiy@gmail.com>
2023-10-23 00:15:31 +0300

    я_опять_что-то_поломал

---
+++ src/main/java/edu/project1/Dictionary.java
@@ -0,0 +1,11 @@
+package edu.project1;
+
+import java.io.IOException;
+
+public interface Dictionary {
+    String getRandomWord();
+
+    Dictionary setDictionary(String dict) throws IOException;
+}
+
+
